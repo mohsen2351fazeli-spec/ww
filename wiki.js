a863a0c57d4c3b73519f8b06b71938b5f5a7d780
Mohsen Fazeli <mohsen2351fazeli@gmail.com>
2026-02-13 03:08:40 -0800

    Add files via upload

---
+++ wiki.js
@@ -0,0 +1,402 @@
+const godata = {
+  ahs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./ahs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Adolf Hitler",
+    "آدولف هیتلر",
+  ],
+  wcs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./wcs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Winston Churchil",
+    "وینستون چرچیل",
+  ],
+  ers: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./ers.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Erwin rommel",
+    "اروین رومل",
+  ],
+  frs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./frs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Franklin Roosevelt",
+    "فرانکلین روزولت",
+  ],
+  ugs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./ugs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Joseph Goebbels",
+    "جوزف گوبلز",
+  ],
+  gss: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./gss.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Joseph Stalin",
+    "جوزف استالین",
+  ],
+  hgs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./hgs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Hermann Goring",
+    "هرمان گورینگ",
+  ],
+  hhs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./hhs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Heinrich Himmler",
+    "هاینریش هیملر",
+  ],
+  sds: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./sds.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Charles de Gaule",
+    "شارل دوگل",
+  ],
+  das: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./das.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Dwight Eisenhower",
+    "دوایت آیزنهاور",
+  ],
+  dms: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./dms.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Douglas MacArthur",
+    "داگلاس مک آرتور",
+  ],
+  mms: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./mms.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Adolf Hitler",
+    "آدولف هیتلر",
+  ],
+  gzs: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./gzs.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Adolf Hitler",
+    "آدولف هیتلر",
+  ],
+  kss: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./kss.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Adolf Hitler",
+    "آدولف هیتلر",
+  ],
+  tiger: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./tiger.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "tiger",
+    "تایگر",
+  ],
+  spitfire: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./spitfire.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "spitfire",
+    "سپیت فایر",
+  ],
+  b17: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./b17.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "b17",
+    "بی 17",
+  ],
+  mg42: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./mg42.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "mg42",
+    "ام جی 42",
+  ],
+  ppsh: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./ppsh.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "ppsh",
+    "پی پی اس اچ",
+  ],
+  t34: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./t34.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "t34",
+    "تی 34",
+  ],
+  v2: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./v2.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "v2",
+    "وی 2",
+  ],
+  me262: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./me262.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "me262",
+    "ام ای 262",
+  ],
+  flak: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./flak.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "flak-wagon",
+    "فلک واگن",
+  ],
+  u: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./u.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "u-boat",
+    "قایق های یو",
+  ],
+  enigma: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./enigma.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "enigma",
+    "انیگما",
+  ],
+  fatman: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./fatman.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "fatman",
+    "مرد چاق",
+  ],
+  RiseoftheThirdReich: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./RiseoftheThirdReich.jpg')] h-[30rem] bg-bottom relative flex flex-col items-center",
+    "Rise of the Third Reich",
+    "ظهور رایش سوم",
+  ],
+  nschluss: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./nschluss.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Anschluss",
+    "آنشلوس",
+  ],
+  MunichConference: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./MunichConference.jpg')] h-[30rem] bg-center relative flex flex-col items-center",
+    "Munich Conference",
+    "کنفرانس مونیخ",
+  ],
+  DismantlingofCzechoslovakia: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./DismantlingofCzechoslovakia.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Dismantling of Czechoslovakia",
+    "تجزیه چکسلواکی",
+  ],
+  Poland: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Poland.jpg')] h-[30rem] bg-center relative flex flex-col items-center",
+    "The Danzig Crisis & Invasion of Poland",
+    "بحران دانتزیگ و تهاجم به لهستان",
+  ],
+  Danmark: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Danmark.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Only Six Hours : Fall of Danmark",
+    "فقط شش ساعت: سقوط دانمارک",
+  ],
+  TheNorthernCampaign: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./TheNorthernCampaign.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "The Northern Campaign",
+    "لشکرکشی شمالی",
+  ],
+  West: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./West.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Blitz in the West",
+    "حمله برق‌آسا در غرب",
+  ],
+  Dunkirk: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Dunkirk.jpg')] h-[30rem] bg-bottom relative flex flex-col items-center",
+    "Dunkirk",
+    "دانکرک",
+  ],
+  Fuhrer: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Fuhrer.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Führer in Paris",
+    "پیشوا در پاریس",
+  ],
+  TheBattleofBritain: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./TheBattleofBritain.jpg')] h-[30rem] bg-right relative flex flex-col items-center",
+    "The Battle of Britain",
+    "نبرد بریتانیا",
+  ],
+  Greece: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Greece.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Yugoslavia & Greece",
+    "یوگسلاوی و یونان",
+  ],
+  Barbarossa: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Barbarossa.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Barbarossa",
+    "بارباروسا",
+  ],
+  Stalingrad: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Stalingrad.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Battle of Stalingrad",
+    "نبرد استالینگراد",
+  ],
+  Leningrad: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Leningrad.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "The Siege of Leningrad",
+    "محاصره لنینگراد",
+  ],
+  rr: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./rr.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Battle of El Alamein",
+    "نبرد العلمین",
+  ],
+  ss: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./ss.jpg')] h-[30rem] bg-left relative flex flex-col items-center",
+    "The End of the Desert Fox",
+    "پایان روباه صحرا",
+  ],
+  Sicily: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./Sicily.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Invasion of Sicily",
+    "تهاجم به سیسیل",
+  ],
+  TheFallofMussolini: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./TheFallofMussolini.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "The Fall of Mussolini",
+    "سقوط موسولینی",
+  ],
+  dday: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./dday.webp')] h-[30rem] bg-center  relative flex flex-col items-center",
+    "D-Day",
+    "روز دی",
+  ],
+  LastHope: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./LastHope.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Ardennes: Last Hope",
+    "آردن: آخرین امید",
+  ],
+  end: [
+    "wiki-main bg-no-repeat bg-cover  bg-[url('./end.jpg')] h-[30rem] bg-top relative flex flex-col items-center",
+    "Battle of Berlin: the End",
+    "نبرد برلین: پایان",
+  ],
+};
+
+const qq = document.querySelector(".panel");
+const panelbtn = document.querySelector(".panel-btn");
+const hidebtn = document.querySelector(".hide-panel");
+const panel = document.querySelector(".panel");
+const Chractersmein = document.querySelector(".Chracters-main");
+const Chractersbtn = document.querySelector(".Chracters-main-btn");
+const df = document.querySelector(".df");
+const lf = document.querySelector(".lf");
+const ef = document.querySelector(".ef");
+const ff = document.querySelector(".ff");
+
+// const battelsmein=document.querySelector(".battels-main")
+// const battelsbtn=document.querySelector(".battels-main-btn")
+
+const darkmein = document.querySelector(".dark-main");
+const darkbtn = document.querySelector(".darkk-main-btn");
+
+const languagemein = document.querySelector(".language-main");
+const languagebtn = document.querySelector(".language-main-btn");
+
+const wipenmein = document.querySelector(".wipen-main");
+const wipenbtn = document.querySelector(".wipen-main-btn");
+
+const dark = document.querySelector(".d-btn");
+const light = document.querySelector(".l-btn");
+const html = document.querySelector(".htmml");
+
+const wikiText = document.querySelector(".wiki-text");
+const wikiTitr = document.querySelector(".wiki-titr");
+const wikiMain = document.querySelector(".wiki-main");
+const goes = document.querySelectorAll(".go");
+
+const textHandler = () => {
+  const data = JSON.parse(localStorage.getItem("godata"));
+  const ise = JSON.parse(localStorage.getItem("ise"));
+  wikiTitr.innerHTML = data[1];
+  wikiMain.className = data[0];
+  if (ise) wikiTitr.innerHTML = data[1];
+  else wikiTitr.innerHTML = data[2];
+};
+const panelHandler = () => {
+  panel.classList.toggle("w-0");
+  panel.classList.toggle("w-[50vw]");
+  if (Chractersbtn.classList.contains("rotate-45")) ChractersHandler();
+  if (darkbtn.classList.contains("rotate-45")) darkkHandler();
+  if (wipenbtn.classList.contains("rotate-45")) wipenHandler();
+  if (languagebtn.classList.contains("rotate-45")) languageHandler();
+};
+const ChractersHandler = () => {
+  Chractersbtn.classList.toggle("-rotate-[135deg]");
+  Chractersbtn.classList.toggle("rotate-45");
+  Chractersmein.classList.toggle("h-0");
+  Chractersmein.classList.toggle("p-2");
+  Chractersmein.classList.toggle("overflow-y-hidden");
+};
+// const battelsHandler=()=>{
+//     battelsbtn.classList.toggle("-rotate-[135deg]")
+//     battelsbtn.classList.toggle("rotate-45")
+//     battelsmein.classList.toggle("h-0")
+//     battelsmein.classList.toggle("p-2")
+//     battelsmein.classList.toggle("overflow-y-hidden")
+// }
+const wipenHandler = () => {
+  wipenbtn.classList.toggle("-rotate-[135deg]");
+  wipenbtn.classList.toggle("rotate-45");
+  wipenmein.classList.toggle("h-0");
+  wipenmein.classList.toggle("p-2");
+  wipenmein.classList.toggle("overflow-y-hidden");
+};
+const languageHandler = () => {
+  languagebtn.classList.toggle("-rotate-[135deg]");
+  languagebtn.classList.toggle("rotate-45");
+  languagemein.classList.toggle("h-0");
+  languagemein.classList.toggle("p-2");
+  languagemein.classList.toggle("overflow-y-hidden");
+};
+const darkkHandler = () => {
+  darkbtn.classList.toggle("-rotate-[135deg]");
+  darkbtn.classList.toggle("rotate-45");
+  darkmein.classList.toggle("h-0");
+  darkmein.classList.toggle("p-2");
+  darkmein.classList.toggle("overflow-y-hidden");
+};
+const goHandler = () => {
+  goes.forEach((g) => {
+    g.addEventListener("click", (event) => {
+      const name = event.target.dataset.name;
+      localStorage.setItem("godata", JSON.stringify(godata[name]));
+      location.assign("./wiki.html");
+    });
+  });
+};
+const lHandler = () => {
+  setTimeout(() => {
+    const data = JSON.parse(localStorage.getItem("godata"));
+    const k = {
+      en: ["Chracters", "weapon", "dark mod", "language"],
+      fa: ["شخصیت ها", "تسلیحات", "حالت تاریک", "زبان"],
+    };
+    const ise = JSON.parse(localStorage.getItem("ise"));
+    if (!ise) {
+      const phs = document.querySelectorAll(".ph");
+      phs[0].innerHTML = k.fa[0];
+      phs[1].innerHTML = k.fa[1];
+      phs[2].innerHTML = k.fa[2];
+      phs[3].innerHTML = k.fa[3];
+      df.innerHTML = "تاریک";
+      lf.innerHTML = "روشن";
+      ef.innerHTML = "انگلیسی";
+      ff.innerHTML = "فارسی";
+      wikiTitr.innerHTML = data[2];
+    } else {
+      const phs = document.querySelectorAll(".ph");
+      phs[0].innerHTML = k.en[0];
+      phs[1].innerHTML = k.en[1];
+      phs[2].innerHTML = k.en[2];
+      phs[3].innerHTML = k.en[3];
+      df.innerHTML = "dark";
+      lf.innerHTML = "light";
+      ef.innerHTML = "EN";
+      ff.innerHTML = "FA";
+      wikiTitr.innerHTML = data[1];
+    }
+  }, 400);
+};
+dark.addEventListener("click", () => {
+  localStorage.setItem("isdark", JSON.stringify(true));
+  darkHandler();
+});
+light.addEventListener("click", () => {
+  localStorage.setItem("isdark", JSON.stringify(false));
+  darkHandler();
+});
+const darkHandler = () => {
+  if (JSON.parse(localStorage.getItem("isdark"))) html.classList.add("dark");
+  else html.classList.remove("dark");
+};
+ef.addEventListener("click", () => {
+  localStorage.setItem("ise", JSON.stringify(true));
+  lHandler();
+  panelHandler();
+});
+ff.addEventListener("click", () => {
+  localStorage.setItem("ise", JSON.stringify(false));
+  lHandler();
+
+  panelHandler();
+});
+
+panelbtn.addEventListener("click", panelHandler);
+hidebtn.addEventListener("click", panelHandler);
+Chractersbtn.addEventListener("click", ChractersHandler);
+// battelsbtn.addEventListener("click",battelsHandler)
+wipenbtn.addEventListener("click", wipenHandler);
+darkbtn.addEventListener("click", darkkHandler);
+languagebtn.addEventListener("click", languageHandler);
+textHandler();
+goHandler();
+darkHandler();
